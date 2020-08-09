f9d74d746be357e9b4af54ddf88843de7b264a26
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-08-09 05:14:30 +0200

    Create Simple node module

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,14 +1,11 @@
-let rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+let rect = require("./rectangle");
 
 function solveRect(l, b) {
   console.log("Solving for rectangle with l = " + l + " and for b = " + b);
 
   if (l <= 0 || b <= 0) {
     console.log(
-      "Rectangle dimensions should be greater than zer: l = " +
+      "Rectangle dimensions should be greater than zero : l = " +
         l +
         ", and b = " +
         b
